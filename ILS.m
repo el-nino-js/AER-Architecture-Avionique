ad32b8a47f99b2423e81db8df483e0f8f63adfee
Nuno <nuno.silvapinto@outlook.com>
2021-03-17 15:21:27 -0400

    Add function template

---
+++ ILS.m
@@ -0,0 +1,15 @@
+%% ILS.m
+% Résout une addition et une soustraction de deux nombres
+% Entrés:
+%   x: nombre reel    
+%   y: nombre reel
+%
+% Sortie:
+%   z: somme des deux nombres
+%   w: different des deux nombre
+function [z,w] = ILS()
+    Donnee();
+    z = x + y;
+    w = x - y;
+end
+
