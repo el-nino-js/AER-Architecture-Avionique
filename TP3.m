625939699f90e9dd675ebd9498e1a6502b8868fd
Nuno <nuno.silvapinto@outlook.com>
2021-03-17 14:54:17 -0400

    Main file

---
+++ TP3.m
@@ -0,0 +1,22 @@
+%**********************************************************************
+% NOM : AER8721.m                                                     *
+%**********************************************************************
+% FONCTION : CE PROGRAMME LIT UN FICHIER *.CSV ET TRACE DES           *
+%            GRAPHIQUES 2D et 3D. SI LE FICHIER *.CSV CONTIENT LA     *
+%            POSITION DE L'AVION, LE PROGRAMME TRACE :                *
+%                   - LA NAV LATÉRALE DU VOL (2D)                     *
+%                   - LA NAV VERTICALE DU VOL (2D)                    *
+%                   - LA NAV COMPLÈTE (3D)                            *
+%**********************************************************************
+% FICHIER *.CSV REQUIS DANS LE MÊME DOSSIER                           *
+%**********************************************************************
+%  AUTEUR : Nuno Silva-Pinto                                              *
+%**********************************************************************
+%  DATE DE CRÉATION : 17 MARS 2021. MODIFIE 31 OCT 2018               *
+%**********************************************************************
+
+clear all;
+clc;
+clf;
+
+disp('ICD')
